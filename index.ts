2e97bb128f11c10863ee2c3f06a8f2ca530e19e2
Rasmus Eskola <fruitiex@gmail.com>
2019-05-05 21:09:14 +0300

    Implement queue timeouts

--- index.ts
+++ index.ts
@@ -8,6 +8,9 @@ dotenv.config();
 import { Lens } from "monocle-ts";
 
 const DEFAULT_QUEUE = process.env.DEFAULT_QUEUE || "5v5";
+const TIMEOUT = process.env.TIMEOUT
+  ? parseInt(process.env.TIMEOUT)
+  : 1000 * 60 * 60;
 
 if (!process.env.TOKEN)
   throw new Error(
@@ -17,6 +20,7 @@ if (!process.env.TOKEN)
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 interface Queue {
+  timeout?: NodeJS.Timer;
   [userId: number]: string | undefined;
 }
 
@@ -30,14 +34,39 @@ interface Channels {
 
 let state: Channels = {};
 
+/**
+ * Updates the bot's state
+ *
+ * @param newState Set this value as the new state
+ */
+const updateState = (newState: Channels) => {
+  state = newState;
+};
+
+/**
+ * Get number of players until given queue is full
+ *
+ * @param queueId Queue ID
+ */
 const getMaxPlayers = (queueId: string) => {
   const maxPlayers = parseInt(queueId[0]);
   return isNaN(maxPlayers) ? 5 : maxPlayers;
 };
 
+/**
+ * Get username of user (falls back to first name/last name)
+ *
+ * @param from Telegram user object
+ */
 const getUsername = (from: TelegramBot.User) =>
   from.username ? `@${from.username}` : `${from.first_name} ${from.last_name}`;
 
+/**
+ * Returns number of players in given queue
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ */
 const getNumPlayers = (channelId: number, queueId: string) => {
   const channelLens = Lens.fromNullableProp<Channels>()(channelId, {});
   const queueLens = Lens.fromNullableProp<Channel>()(queueId, {});
@@ -45,6 +74,13 @@ const getNumPlayers = (channelId: number, queueId: string) => {
   return Object.keys(channelLens.compose(queueLens).get(state)).length;
 };
 
+/**
+ * Returns printable list of players in given queue.
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ * @param avoidHighlight Whether to avoid highlighting players (removes @-prefix)
+ */
 const getPlayersStr = (
   channelId: number,
   queueId: string,
@@ -60,6 +96,12 @@ const getPlayersStr = (
     .join(", ");
 };
 
+/**
+ * Logs status of given queue.
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ */
 const logStatus = (channelId: number, queueId: string) => {
   const numPlayers = getNumPlayers(channelId, queueId);
 
@@ -75,7 +117,61 @@ const logStatus = (channelId: number, queueId: string) => {
   }
 };
 
-const add = (
+/**
+ * Called when a queue times out. Resets the given queue by clearing out all
+ * players.
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ */
+const timeoutQueue = (channelId: number, queueId: string) => () => {
+  const channelLens = Lens.fromNullableProp<Channels>()(channelId, {});
+  const queueLens = Lens.fromNullableProp<Channel>()(queueId, {});
+  const numPlayers = getNumPlayers(channelId, queueId);
+
+  if (numPlayers > 0) {
+    bot.sendMessage(channelId, `${queueId} timed out after inactivity.`);
+  }
+
+  updateState(channelLens.compose(queueLens).set({})(state));
+};
+
+/**
+ * Clears and optionally sets queue timeout
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ * @param restart Whether to restart the timer after clearing it
+ */
+const updateTimeout = (
+  channelId: number,
+  queueId: string,
+  restart?: boolean
+) => {
+  const channelLens = Lens.fromNullableProp<Channels>()(channelId, {});
+  const queueLens = Lens.fromNullableProp<Channel>()(queueId, {});
+  const timeoutLens = Lens.fromNullableProp<Queue>()("timeout", undefined);
+
+  const lens = channelLens.compose(queueLens).compose(timeoutLens);
+
+  clearTimeout(lens.get(state));
+
+  if (restart) {
+    updateState(
+      lens.set(setTimeout(timeoutQueue(channelId, queueId), TIMEOUT))(state)
+    );
+  }
+};
+
+/**
+ * Adds player to given queue.
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ * @param userId User ID
+ * @param userName Telegram username or first name/last name
+ */
+const addToQueue = (
   channelId: number,
   queueId: string,
   userId: number,
@@ -85,10 +181,12 @@ const add = (
   const queueLens = Lens.fromNullableProp<Channel>()(queueId, {});
   const userLens = Lens.fromNullableProp<Queue>()(userId, undefined);
 
-  state = channelLens
-    .compose(queueLens)
-    .compose(userLens)
-    .set(userName)(state);
+  updateState(
+    channelLens
+      .compose(queueLens)
+      .compose(userLens)
+      .set(userName)(state)
+  );
 
   const numPlayers = getNumPlayers(channelId, queueId);
   const maxPlayers = getMaxPlayers(queueId);
@@ -99,21 +197,36 @@ const add = (
       `Game ready! ${getPlayersStr(channelId, queueId)}`
     );
 
-    state = channelLens.compose(queueLens).set({})(state);
+    updateTimeout(channelId, queueId, false);
+    updateState(channelLens.compose(queueLens).set({})(state));
   } else {
+    updateTimeout(channelId, queueId, true);
     logStatus(channelId, queueId);
   }
 };
 
-const remove = (channelId: number, queueId: string, userId: number) => {
+/**
+ * Remove player from given queue.
+ *
+ * @param channelId Telegram group
+ * @param queueId Queue ID
+ * @param userId User ID
+ */
+const removeFromQueue = (
+  channelId: number,
+  queueId: string,
+  userId: number
+) => {
   const channelLens = Lens.fromNullableProp<Channels>()(channelId, {});
   const queueLens = Lens.fromNullableProp<Channel>()(queueId, {});
 
-  state = channelLens.compose(queueLens).modify(queue => {
-    const copy = { ...queue };
-    delete copy[userId];
-    return copy;
-  })(state);
+  updateState(
+    channelLens.compose(queueLens).modify(queue => {
+      const copy = { ...queue };
+      delete copy[userId];
+      return copy;
+    })(state)
+  );
 
   logStatus(channelId, queueId);
 };
@@ -128,11 +241,11 @@ bot.onText(telegramBotRe, ({ from, chat }, match) => {
 
   switch (cmd) {
     case "add":
-      add(chat.id, queueId, from.id, getUsername(from));
+      addToQueue(chat.id, queueId, from.id, getUsername(from));
       break;
 
     case "remove":
-      remove(chat.id, queueId, from.id);
+      removeFromQueue(chat.id, queueId, from.id);
       break;
 
     case "status":
